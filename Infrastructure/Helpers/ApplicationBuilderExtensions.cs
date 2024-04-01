18ee8ed4ebb00739f4e7be0e5424bb85d00c2f0b
Gala <70331948+galinamiteva@users.noreply.github.com>
2024-04-01 14:52:07 +0200

    Infrastructure

---
+++ Infrastructure/Helpers/ApplicationBuilderExtensions.cs
@@ -0,0 +1,14 @@
+﻿
+
+using Microsoft.AspNetCore.Builder;
+
+namespace Infrastructure.Helpers;
+
+public static class ApplicationBuilderExtensions
+{
+
+    public static IApplicationBuilder UseUserSessionValidation(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<UserSessionValidationMiddleware>();
+    }
+}
