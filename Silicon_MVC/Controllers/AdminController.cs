77e7196b242c3e99c87dd7e09ea9e61e9ddcefc4
Gala <70331948+galinamiteva@users.noreply.github.com>
2024-04-21 20:41:37 +0200

    update

--- Silicon_MVC/Controllers/AdminController.cs
+++ Silicon_MVC/Controllers/AdminController.cs
@@ -1,7 +1,9 @@
-﻿using Infrastructure.Helpers;
+﻿using Infrastructure.Entities;
+using Infrastructure.Helpers;
 using Infrastructure.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
@@ -16,14 +18,13 @@ using static System.Net.WebRequestMethods;
 namespace Silicon_MVC.Controllers;
 
 [Authorize(Policy = "Admins")]
-public class AdminController(HttpClient http,IConfiguration configuration, HttpClient httpClient, CategoryIdAssigner categoryIdAssigner, IHttpClientFactory httpClientFactory) : Controller
+public class AdminController(UserManager<UserEntity> userManager, HttpClient http,IConfiguration configuration, CategoryIdAssigner categoryIdAssigner, IHttpClientFactory httpClientFactory) : Controller
 {
-    private readonly HttpClient _httpClient = httpClient;
     private readonly IConfiguration _configuration = configuration;
     private readonly CategoryIdAssigner _categoryIdAssigner = categoryIdAssigner;
     private readonly IHttpClientFactory _httpClientFactory = httpClientFactory;
     private readonly HttpClient _http = http;
-
+    private readonly UserManager<UserEntity> _userManager = userManager;
 
     public IActionResult Index()
     {
@@ -81,7 +82,7 @@ public class AdminController(HttpClient http,IConfiguration configuration, HttpC
 
             var json = JsonConvert.SerializeObject(viewModel);
             using var content = new StringContent(json, Encoding.UTF8, "application/json");
-            var categoryResponse = await _httpClient.GetAsync($"https://localhost:7029/api/Category/{viewModel.Course.Category}?key={_configuration["ApiKey:Secret"]}");
+            var categoryResponse = await _http.GetAsync($"https://localhost:7029/api/Category/{viewModel.Course.Category}?key={_configuration["ApiKey:Secret"]}");
 
             if (categoryResponse.IsSuccessStatusCode)
             {
@@ -92,7 +93,7 @@ public class AdminController(HttpClient http,IConfiguration configuration, HttpC
             
             
             var contentCourse = new StringContent(JsonConvert.SerializeObject(viewModel.Course), Encoding.UTF8, "application/json");
-            var response = await _httpClient.PutAsync($"https://localhost:7029/api/Courses?key={_configuration["ApiKey:Secret"]}", contentCourse);
+            var response = await _http.PutAsync($"https://localhost:7029/api/Courses?key={_configuration["ApiKey:Secret"]}", contentCourse);
             if (response.IsSuccessStatusCode)
             {
                 ViewData["CourseStatus"] = "Course updated succesfully";
@@ -126,7 +127,7 @@ public class AdminController(HttpClient http,IConfiguration configuration, HttpC
 
         try
         {
-            var responseCategory = await _httpClient.GetAsync($"https://localhost:7029/api/Courses?category={Uri.EscapeDataString(category)}&searchQuery={Uri.EscapeDataString(searchQuery)}&key={_configuration["ApiKey:Secret"]}");
+            var responseCategory = await _http.GetAsync($"https://localhost:7029/api/Courses?category={Uri.EscapeDataString(category)}&searchQuery={Uri.EscapeDataString(searchQuery)}&key={_configuration["ApiKey:Secret"]}");
             if (responseCategory.IsSuccessStatusCode)
             {
                 var jsonStrings = await responseCategory.Content.ReadAsStringAsync();
@@ -139,7 +140,7 @@ public class AdminController(HttpClient http,IConfiguration configuration, HttpC
             }
 
             var contentCourse = new StringContent(JsonConvert.SerializeObject(viewModel.Course), Encoding.UTF8, "application/json");
-            var response = await _httpClient.PutAsync($"https://localhost:7029/api/Courses?key={_configuration["ApiKey:Secret"]}", contentCourse);
+            var response = await _http.PutAsync($"https://localhost:7029/api/Courses?key={_configuration["ApiKey:Secret"]}", contentCourse);
             if (response.IsSuccessStatusCode)
             {
                 ViewData["Status"] = "Success"; ;
@@ -164,28 +165,59 @@ public class AdminController(HttpClient http,IConfiguration configuration, HttpC
 
 
 
-    //[HttpPost("/admin/courses/delete")]
-    public async Task<IActionResult> Delete(AdminCoursesViewModel viewModel)
+    //public async Task<IActionResult> Delete(AdminCoursesViewModel viewModel)
+    //{
+    //    //_httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", HttpContext.Session.GetString("token"));
+
+    //    var Id = viewModel.Course.Id;
+    //    var cancellationTokenSource = new CancellationTokenSource();
+    //    var cancellationToken = cancellationTokenSource.Token;
+
+    //    var response = await _httpClient.DeleteAsync($"https://localhost:7029/api/Courses/{Id}?key={_configuration["ApiKey:Secret"]}", cancellationToken);
+    //    if (response.IsSuccessStatusCode)
+    //    {
+    //        ViewData["CourseStatus"] = "Course deleted succesfully";
+    //        return RedirectToAction("Courses");
+    //    }
+    //    else
+    //    {
+    //        ViewData["CourseStatus"] = response.StatusCode;
+    //        return RedirectToAction("Courses");
+    //    }
+    //}
+
+    [HttpPost]
+    public async Task<IActionResult> Delete(int courseId)
     {
-        //_httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", HttpContext.Session.GetString("token"));
-
-        var Id = viewModel.Course.Id;
-        var cancellationTokenSource = new CancellationTokenSource();
-        var cancellationToken = cancellationTokenSource.Token;
-
-        var response = await _httpClient.DeleteAsync($"https://localhost:7029/api/Courses/{Id}?key={_configuration["ApiKey:Secret"]}", cancellationToken);
-        if (response.IsSuccessStatusCode)
+        try
         {
-            ViewData["CourseStatus"] = "Course deleted succesfully";
-            return RedirectToAction("Courses");
+            var user = await _userManager.GetUserAsync(User);
+            var userId = user!.Id;
+
+
+
+            var url = $"https://localhost:7029/api/UserCourses/{userId}/{courseId}?key={_configuration["ApiKey:Secret"]}";
+
+
+            var response = await _http.DeleteAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                TempData["SuccessMessage"] = "Course removed successfully.";
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Failed to remove course.";
+            }
+
+            return RedirectToAction(nameof(Courses));
         }
-        else
+        catch (Exception ex)
         {
-            ViewData["CourseStatus"] = response.StatusCode;
-            return RedirectToAction("Courses");
+            Console.WriteLine($"Error: {ex.Message}");
+            TempData["ErrorMessage"] = "Failed to remove course. Please try again later.";
+            return RedirectToAction(nameof(Courses));
         }
     }
-
-
 
 }
--- Silicon_MVC/Controllers/CoursesController.cs
+++ Silicon_MVC/Controllers/CoursesController.cs
@@ -1,6 +1,10 @@
-﻿using Infrastructure.Models;
+﻿using Infrastructure.Entities;
+using Infrastructure.Helpers;
+using Infrastructure.Migrations;
+using Infrastructure.Models;
 using Infrastructure.Services;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Newtonsoft.Json;
@@ -8,7 +12,9 @@ using Silicin_MVC.Services;
 using Silicon_MVC.Models;
 using Silicon_MVC.Models.Views;
 using Silicon_MVC.ViewModels;
+using Silicon_MVC.ViewModels.Admin;
 using Silicon_MVC.ViewModels.Components;
+using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using static System.Net.WebRequestMethods;
@@ -18,7 +24,7 @@ namespace Silicon_MVC.Controllers;
 
 
 [Authorize]
-public class CoursesController(HttpClient http,CategoryService categoryService, CourseService courseService, IConfiguration configuration) : Controller
+public class CoursesController(CategoryIdAssigner categoryIdAssigner, UserManager<UserEntity> userManager, HttpClient http,CategoryService categoryService, CourseService courseService, IConfiguration configuration) : Controller
 {
 
 
@@ -26,6 +32,9 @@ public class CoursesController(HttpClient http,CategoryService categoryService, 
     private readonly CourseService _courseService = courseService;
     private readonly IConfiguration _configuration = configuration;
     private readonly HttpClient _http = http;
+    private readonly UserManager<UserEntity> _userManager = userManager;
+    private readonly CategoryIdAssigner _categoryIdAssigner = categoryIdAssigner;
+
 
 
     public async Task<IActionResult> Index(string category = "", string searchQuery = "", int pageNumber = 1, int pageSize = 6)
@@ -111,7 +120,78 @@ public class CoursesController(HttpClient http,CategoryService categoryService, 
     }
 
 
+    [HttpPost]
+    public async Task<IActionResult> Delete(int courseId)
+    {
+        try
+        {
+            var user = await _userManager.GetUserAsync(User);
+            var userId = user!.Id;
 
 
+
+            var url = $"https://localhost:7029/api/UserCourses/{userId}/{courseId}?key={_configuration["ApiKey:Secret"]}";
+
+
+            var response = await _http.DeleteAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                TempData["SuccessMessage"] = "Course removed successfully.";
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Failed to remove course.";
+            }
+
+            return RedirectToAction(nameof(Courses));
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+            TempData["ErrorMessage"] = "Failed to remove course. Please try again later.";
+            return RedirectToAction(nameof(Courses));
+        }
+    }
+
+
+    public async Task<IActionResult> Courses(string category = "", string searchQuery = "")
+    {
+        var viewModel = new AdminCoursesViewModel();
+
+        try
+        {
+            var responseCategory = await _http.GetAsync($"https://localhost:7029/api/Courses?category={Uri.EscapeDataString(category)}&searchQuery={Uri.EscapeDataString(searchQuery)}&key={_configuration["ApiKey:Secret"]}");
+            if (responseCategory.IsSuccessStatusCode)
+            {
+                var jsonStrings = await responseCategory.Content.ReadAsStringAsync();
+                var courses = JsonConvert.DeserializeObject<CourseResultModel>(jsonStrings);
+                if (courses != null)
+                {
+                    viewModel.Courses = courses.Courses!;
+                    return View(viewModel);
+                }
+            }
+
+            var contentCourse = new StringContent(JsonConvert.SerializeObject(viewModel.Course), Encoding.UTF8, "application/json");
+            var response = await _http.PutAsync($"https://localhost:7029/api/Courses?key={_configuration["ApiKey:Secret"]}", contentCourse);
+            if (response.IsSuccessStatusCode)
+            {
+                ViewData["Status"] = "Success"; ;
+            }
+            else
+            {
+                ViewData["Status"] = "ConnectionFailed";
+                ViewData["StatusCode"] = (int)response.StatusCode;
+            }
+        }
+        catch
+        {
+            ViewData["Status"] = "ConnectionFailed";
+        }
+        return View();
+
+
+    }
 
 }
